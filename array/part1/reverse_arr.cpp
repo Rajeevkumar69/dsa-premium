c87391df7cba62796d3ace3a4b82603bf00a1d39
Rajeevkumar69 <rk488296@gmail.com>
2026-08-05 02:04:52 +0530

    reverse using 2 pointers

--- array/part1/reverse_arr.cpp
+++ array/part1/reverse_arr.cpp
@@ -6,9 +6,26 @@ int main()
      int arr[] = {5, 4, 3, 9, 2};
      int n = sizeof(arr) / sizeof(int);
 
+     // Linear search
+
      for (int i = n - 1; i >= 0; i--)
      {
           cout << arr[i] << " ";
+     }
+     cout << "\n\n\n";
+
+     //  Two pointers approach
+     int st = 0, end = n - 1;
+
+     while (st < end)
+     {
+          swap(arr[st], arr[end]);
+          st++;
+          end--;
+     }
+     for (int i = 0; i < n; i++)
+     {
+          cout << arr[i] << ",";
      }
      return 0;
 }
