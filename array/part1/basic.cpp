8a266858bcab9c317b7c3a89df636b04f684d26f
Rajeevkumar69 <rk488296@gmail.com>
2026-08-05 00:14:21 +0530

    array basic

--- array/part1/basic.cpp
+++ array/part1/basic.cpp
@@ -5,8 +5,14 @@ int main()
 {
      int arr[] = {10, 20, 30, 50, 120};
 
+     // Formula to calculate the length of array
      int n = sizeof(arr) / sizeof(int);
 
      cout << "Size of array is: " << n;
+
+     for (int i = 0; i < n; i++)
+     {
+          cout << arr[i] << " ";
+     }
      return 0;
 }
