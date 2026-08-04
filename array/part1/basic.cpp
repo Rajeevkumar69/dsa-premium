adc2117e2470b8d3a91030c14cac78d3acc79cdb
Rajeevkumar69 <rk488296@gmail.com>
2026-08-05 00:19:10 +0530

    array basic

--- array/part1/basic.cpp
+++ array/part1/basic.cpp
@@ -3,12 +3,20 @@ using namespace std;
 
 int main()
 {
-     int arr[] = {10, 20, 30, 50, 120};
+     int arr[5];
 
      // Formula to calculate the length of array
      int n = sizeof(arr) / sizeof(int);
 
-     cout << "Size of array is: " << n;
+     cout << "Size of array is: " << n << "\n";
+
+     // Input in array
+     cout << "Enter Student Marks:- \n";
+
+     for (int i = 0; i < n; i++)
+     {
+          cin >> arr[i];
+     }
 
      for (int i = 0; i < n; i++)
      {
