fdcb9fe0fbffb4b89b9c63b47507aca15c168762
Rajeevkumar69 <rk488296@gmail.com>
2026-08-16 10:44:34 +0530

    Q2: Selection Sort

--- array/sorting_algo/assignment.cpp
+++ array/sorting_algo/assignment.cpp
@@ -22,15 +22,59 @@ vector<int> bubbleSort(vector<int> &nums, int n)
      return nums;
 }
 
+/*Q1: Use the selection sort algorithm to sort the
+array in descending order
+*/
+
+vector<int> selectionSort(vector<int> &nums, int n)
+{
+     for (int i = 0; i < n - 1; i++)
+     {
+          int minIdxVal = i;
+
+          for (int j = i + 1; j < n; j++)
+          {
+               if (nums[j] > nums[minIdxVal])
+               {
+                    minIdxVal = j;
+               }
+          }
+          swap(nums[i], nums[minIdxVal]);
+     }
+     return nums;
+}
+
 int main()
 {
      vector<int> arr = {3, 6, 2, 1, 8, 7, 4, 5, 3, 1};
      int n = arr.size();
 
-     vector<int> result = bubbleSort(arr, n);
-     for (int val : result)
+     vector<int> result1 = bubbleSort(arr, n);
+     for (int val : result1)
      {
           cout << val << " ";
      }
+     cout << "\n";
+
+     vector<int> result2 = selectionSort(arr, n);
+     for (int val : result2)
+     {
+          cout << val << " ";
+     }
+     cout << "\n";
+
+     vector<int> result3 = insertionSort(arr, n);
+     for (int val : result3)
+     {
+          cout << val << " ";
+     }
+     cout << "\n";
+
+     // vector<int> result4 = countingSort(arr, n);
+     // for (int val : result4)
+     // {
+     //      cout << val << " ";
+     // }
+
      return 0;
 }
