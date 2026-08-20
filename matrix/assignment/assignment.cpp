cc6cd6b0a2bd626718c942c1856f802ce5a19938
Rajeevkumar69 <rk488296@gmail.com>
2026-08-21 00:31:35 +0530

    Q2

--- matrix/assignment/assignment.cpp
+++ matrix/assignment/assignment.cpp
@@ -25,12 +25,32 @@ int countOccurance(int mat[][3], int n, int m, int key)
      return count;
 }
 
+/*
+Q2 : Print out the sum of the numbers in the second
+row of the “nums” array.
+int nums[][] = {{1,4,9},{11,4,3},{2,2,3}};
+Output-18
+*/
+
+int returnSum(int mat[][3], int n, int m)
+{
+     int sum = 0;
+
+     for (int j = 0; j < m; j++)
+     {
+          sum += mat[1][j];
+     }
+
+     return sum;
+}
+
 int main()
 {
-     int arr[2][3] = {{4, 7, 8}, {8, 8, 7}};
-     int n = 2, m = 3;
+     int arr[3][3] = {{1, 4, 9}, {11, 4, 3}, {12, 2, 3}};
+     int n = 3, m = 3;
 
-     int result = countOccurance(arr, n, m, 7);
+     // int result = countOccurance(arr, n, m, 7);
+     int result = returnSum(arr, n, m);
 
      cout << result;
      return 0;
