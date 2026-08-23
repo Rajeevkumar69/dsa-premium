44381caa4e79eeeb48a907b336c73a43e036ffcd
Rajeevkumar69 <rk488296@gmail.com>
2026-08-23 10:23:30 +0530

    char

--- char_arr_strings/intro.cpp
+++ char_arr_strings/intro.cpp
@@ -5,6 +5,15 @@ int main()
 {
      char ch = 'c';
      int position = ch - 97;
-     cout << position;
+     cout << position << " \n";
+
+     char work[] = "code";
+
+     char work2[9] = "chamcham";
+
+     char work3[] = {'c', 'o', 'd', 'e', '\0'};
+
+     char work4[50] = {'c', 'o', 'd', 'e', '\0'};
+     cout << work4;
      return 0;
 }
